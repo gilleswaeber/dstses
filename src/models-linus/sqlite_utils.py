0bd2faa9b777a42f34e1857bf653da34749c7234
Linus Vogel <linusv66@gmail.com>
2021-05-12 12:43:49 +0200

    able to load timeseries from database now

--- src/models-linus/sqlite_utils.py
+++ src/models-linus/sqlite_utils.py
@@ -1,9 +1,10 @@
 import sqlalchemy
+import pandas as pd
 from pathlib import Path
 
 db_file = Path(__file__).parent.parent.absolute() / "hist_data/hist_data.sqlite"
-headers_nabel = [ "PM10", "PM2.5", "Temperature", "Rainfall" ]
-headers_zurich = [ "PM10", "PM2.5", "Humidity", "Temperature", "Pressure" ]
+headers_nabel = ["PM10", "PM2.5", "Temperature", "Rainfall"]
+headers_zurich = ["PM10", "PM2.5", "Humidity", "Temperature", "Pressure"]
 
 
 def get_engine() -> sqlalchemy.engine.Connection:
@@ -13,18 +14,29 @@ def get_engine() -> sqlalchemy.engine.Connection:
 	return sqlalchemy.create_engine(f'sqlite:///{db_file}', echo=False)
 
 
-def get_time_series(engine: sqlalchemy.engine.Connection, dataset: str, location: str):
+def get_time_series(engine: sqlalchemy.engine.Connection, dataset: str, location: str) -> pd.DataFrame:
 	"""
 		extracts a timeseries from the database containing exactly one series of measurements from exactly one location
 	"""
 	# make sure that the dataset exists
 	assert dataset == 'nabel' or dataset == 'zurich'
-	# prefix all headers with the specified location
-	headers = list(map(lambda x: f'\'{location}.{x}\'', headers_nabel if dataset == 'nabel' else headers_zurich))
-	query: str = f'SELECT {",".join(headers)} FROM {dataset} LIMIT 10;'
-	print(query)
-	for a in engine.execute(query).fetchall(): print(a[headers[0]])
-	
 
-# get_time_series(get_engine(), "nabel", "Dübendorf-Empa")
-print(get_engine().execute("SELECT 'Dübendorf-Empa.PM10' FROM nabel LIMIT 1").fetchall())
+	# create list of headers to query
+	headers = ",".join(['"date"'] + [f'"{location}.{x}"' for x in (headers_nabel if dataset == 'nabel' else headers_zurich)])
+	query = f'SELECT {headers} FROM {dataset};'
+	return pd.read_sql_query(query, engine)
+
+
+
+#connection = get_engine().connect()
+#zurich_data: pd.DataFrame = pd.read_sql_table('zurich', connection)
+#nabel_data: pd.DataFrame = pd.read_sql_table('nabel', connection)
+
+#print(nabel_data)
+#print(nabel_data.filter(items=[ "date" ] + list(filter(lambda x: "Dübendorf" in x, nabel_data.columns)), axis=1))
+
+
+#for row in get_engine().execute('SELECT "date","Dübendorf-Empa.PM10","Dübendorf-Empa.PM2.5","Dübendorf-Empa.Temperature","Dübendorf-Empa.Rainfall" FROM nabel LIMIT 10;').fetchall(): print(row)
+#for row in get_engine().execute('SELECT "date","Zch_Stampfenbachstrasse.PM10","Zch_Stampfenbachstrasse.PM2.5","Zch_Stampfenbachstrasse.Humidity","Zch_Stampfenbachstrasse.Temperature","Zch_Stampfenbachstrasse.Pressure" FROM zurich LIMIT 10;').fetchall(): print(row)
+get_time_series(get_engine(), "nabel", "Dübendorf-Empa")
+
