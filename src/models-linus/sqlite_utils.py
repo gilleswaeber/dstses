f6d0c25a3d35548ff5f6e9e38d3ebadf1c86ede5
Linus Vogel <linusv66@gmail.com>
2021-05-12 16:01:55 +0200

    cleaned up some code

--- src/models-linus/sqlite_utils.py
+++ src/models-linus/sqlite_utils.py
@@ -22,21 +22,7 @@ def get_time_series(engine: sqlalchemy.engine.Connection, dataset: str, location
 	assert dataset == 'nabel' or dataset == 'zurich'
 
 	# create list of headers to query
-	headers = ",".join(['"date"'] + [f'"{location}.{x}"' for x in (headers_nabel if dataset == 'nabel' else headers_zurich)])
+	prefixed = [f'"{location}.{x}"' for x in (headers_nabel if dataset == 'nabel' else headers_zurich)]
+	headers = ",".join(['"date"'] + prefixed)
 	query = f'SELECT {headers} FROM {dataset};'
 	return pd.read_sql_query(query, engine)
-
-
-
-#connection = get_engine().connect()
-#zurich_data: pd.DataFrame = pd.read_sql_table('zurich', connection)
-#nabel_data: pd.DataFrame = pd.read_sql_table('nabel', connection)
-
-#print(nabel_data)
-#print(nabel_data.filter(items=[ "date" ] + list(filter(lambda x: "Dübendorf" in x, nabel_data.columns)), axis=1))
-
-
-#for row in get_engine().execute('SELECT "date","Dübendorf-Empa.PM10","Dübendorf-Empa.PM2.5","Dübendorf-Empa.Temperature","Dübendorf-Empa.Rainfall" FROM nabel LIMIT 10;').fetchall(): print(row)
-#for row in get_engine().execute('SELECT "date","Zch_Stampfenbachstrasse.PM10","Zch_Stampfenbachstrasse.PM2.5","Zch_Stampfenbachstrasse.Humidity","Zch_Stampfenbachstrasse.Temperature","Zch_Stampfenbachstrasse.Pressure" FROM zurich LIMIT 10;').fetchall(): print(row)
-get_time_series(get_engine(), "nabel", "Dübendorf-Empa")
-
