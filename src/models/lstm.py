5e2f1b4acf32e8a4950f8e508d872fe3c696ee83
Gilles Waeber <moi@gilleswaeber.ch>
2021-05-27 17:37:12 +0200

    LSTM predictions

--- src/models/lstm.py
+++ src/models/lstm.py
@@ -2,10 +2,12 @@
 	This module contains all code specifically necessary code for the LSTM model
 """
 # disable tensorflow logging
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '4'  # noqa
+
 import json
 import os
 from pathlib import Path
-from typing import List, Tuple
+from typing import List, Tuple, Dict
 
 from pandas import DataFrame
 from tensorflow.keras.optimizers import Adam
@@ -16,15 +18,12 @@ from utils.config import default_config
 from utils.keras import KerasTrainCallback
 from utils.normalizer import Normalizer
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '4'
-
 import tensorflow as tf
-import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
 from tensorflow.keras.models import Sequential, load_model
-from tensorflow.keras.layers import Dense, TimeDistributed
+from tensorflow.keras.layers import Dense, TimeDistributed, Dropout
 from tensorflow.keras.layers import LSTM
 
 from utils.logger import Logger
@@ -45,7 +44,7 @@ class LSTMModel:
 		prepare function and a fit function that does not take pandas DataFrames but numpy ndarrays.
 	"""
 
-	def __init__(self, vars_in: int, vars_out: int, model: Sequential = None):
+	def __init__(self, config: 'LSTMConfig', model: Sequential = None):
 		"""
 			This constructs a LSTMModel for training and usage in a sort of sklearn compatible way.
 
@@ -53,12 +52,15 @@ class LSTMModel:
 
 				vars_in: How many input variables there are
 		"""
+		self.config = config
 
 		if model is None:
 			# initialize model
 			self.model = Sequential()
-			self.model.add(LSTM(units=128, activation='sigmoid', input_shape=(None, vars_in), return_sequences=True))
-			self.model.add(TimeDistributed(Dense(vars_out, activation='sigmoid')))
+			self.model.add(LSTM(units=128, activation='sigmoid', input_shape=(None, len(self.config.features_in)),
+								return_sequences=True))
+			self.model.add(TimeDistributed(Dropout(rate=.1)))
+			self.model.add(TimeDistributed(Dense(len(self.config.features_out), activation='sigmoid')))
 			# self.model.compile(loss='mean_squared_error')
 			self.model.compile(optimizer=Adam(clipnorm=1), loss='mean_squared_error')
 			self.model.summary()
@@ -70,18 +72,21 @@ class LSTMModel:
 		print(f"Shape: {x.shape}")
 		print(f"Model: {self.model.input_shape}")
 		with tqdm(total=epochs, desc='LSTM training', dynamic_ncols=True) as progress:
-			nntc = KerasTrainCallback(progress)
+			nntc = KerasTrainCallback(self.config.name, progress)
 			self.model.fit(x=x, y=y, batch_size=32, validation_split=1 / 8, use_multiprocessing=True, callbacks=[nntc],
 						   epochs=epochs)
 		return self.model
 
-	def predict_next(self, start: np.ndarray) -> np.ndarray:
+	def predict_next_n(self, start: np.ndarray) -> np.ndarray:
+		"""Predict a step in the future (normalized)"""
 		assert len(start.shape) == 2
 		past: np.ndarray = start.reshape((1, *start.shape))
 		y_pred = self.model.predict(past)
 		return y_pred[0, -1, :]
 
-	def predict_sequence(self, start: np.ndarray, length: int) -> np.ndarray:
+	def predict_sequence_n(self, start: np.ndarray, length: int) -> np.ndarray:
+		"""Predict a sequence in the future (normalized)"""
+		assert self.config.use_offset
 		assert len(start.shape) == 2
 		past: np.ndarray = start
 		predicted = []
@@ -91,40 +96,56 @@ class LSTMModel:
 			past = np.append(past, y_pred.reshape((1, -1)), axis=0)
 		return np.array(predicted)
 
+	def predict_n(self, x: np.ndarray) -> np.ndarray:
+		"""Predict corresponding values for each time step (normalized)"""
+		assert len(x.shape) == 2
+		y_pred = self.model.predict(x.reshape(1, *x.shape))
+		return y_pred[0, :, :]
+
 	def store(self, config: SectionProxy):
 		path = Path(config["storage_location"]) / f"{config.name}.pkl"
 		if not os.path.exists(path):
 			self.model.save(path)
 
-	def predict(self, x, fh):
-		return self.predict_next(x)
+	def predict(self, x: DataFrame, fh):
+		pred_df = x.copy()
+		normalizer = Normalizer()
+		for loc, df_loc in split_by_location(x).items():
+			loc_x = df_loc[self.config.features_in].to_numpy()
+			for col, f in enumerate(self.config.features_in):
+				loc_x[:, col] = normalizer.normalize(loc_x[:, col], f)
+			y_n = self.predict_n(loc_x)
+			for col, f in enumerate(self.config.features_out):
+				pred_df[f'{loc}.{f}_Pred'] = normalizer.original(y_n[:, col], f)
+		return pred_df
 
 
-def train_or_load_LSTM(config: SectionProxy, data: pd.DataFrame) -> Tuple[LSTMModel, SectionProxy]:
+def train_or_load_LSTM(config: SectionProxy, data: DataFrame) -> Tuple[LSTMModel, SectionProxy]:
 	path = Path(config["storage_location"]) / f"{config.name}.pkl"
-	if path.exists() and False:
+	if path.exists():
 		return load(config, path), config
 	else:
 		return train_lstm_model_predict(config, data), config
 
 
 def load(config: SectionProxy, path: Path) -> LSTMModel:
-	return LSTMModel(vars_in=0, vars_out=0, model=load_model(path))
+	c = LSTMConfig(config)
+	return LSTMModel(c, model=load_model(path))
 
 
-def split_by_location(df: DataFrame) -> List[DataFrame]:
+def split_by_location(df: DataFrame) -> Dict[str, DataFrame]:
 	"""Split into several dataframes using the the part before the dot in the column name as criterion"""
 	df_cols = list(df.columns)
 	locations = set(c.split('.')[0] for c in df_cols)
-	by_location = []
+	by_location = {}
 	for l in locations:
 		loc_cols = [c for c in df_cols if c.split('.')[0] == l]
-		by_location.append(df[loc_cols].rename(columns={c: c.split('.', 1)[1] for c in loc_cols}))
+		by_location[l] = df[loc_cols].rename(columns={c: c.split('.', 1)[1] for c in loc_cols})
 	return by_location
 
 
 def split_on_gaps(df: DataFrame, gap_gte_seconds) -> List[DataFrame]:
-	"""Drop rows with NaN"""
+	"""Drop rows with NaN and split on data gaps longer than x seconds"""
 	chunks = []
 	df = df.dropna()  # Drop NaNs
 	gaps = ((df.index[1:] - df.index[:-1]).seconds >= gap_gte_seconds).nonzero()[0].tolist()
@@ -138,6 +159,7 @@ def split_on_gaps(df: DataFrame, gap_gte_seconds) -> List[DataFrame]:
 
 class LSTMConfig:
 	def __init__(self, config: SectionProxy):
+		self.name = config.name
 		self.gap_detection = int(config["gap_detection_seconds"])
 		self.stride_length = int(config["train_window"])
 		self.features_in: List[str] = json.loads(config["features_in"])
@@ -145,9 +167,9 @@ class LSTMConfig:
 		self.use_offset: bool = config.getboolean("use_offset")
 
 
-def to_x_y(df: pd.DataFrame, c: LSTMConfig) -> Tuple[np.ndarray, np.ndarray]:
+def to_xy(df: DataFrame, c: LSTMConfig) -> Tuple[np.ndarray, np.ndarray]:
 	by_location = split_by_location(df)
-	chunks = [chunk for l in by_location for chunk in split_on_gaps(l, c.gap_detection) if
+	chunks = [chunk for l in by_location.values() for chunk in split_on_gaps(l, c.gap_detection) if
 			  chunk.shape[0] > c.stride_length]
 
 	# extract the data with a sliding window of length 20
@@ -173,16 +195,16 @@ def to_x_y(df: pd.DataFrame, c: LSTMConfig) -> Tuple[np.ndarray, np.ndarray]:
 	return x, y
 
 
-def train_lstm_model_predict(config: SectionProxy, df: pd.DataFrame) -> LSTMModel:
+def train_lstm_model_predict(config: SectionProxy, df: DataFrame) -> LSTMModel:
 	"""Several things to do here: split by location, do a gap detection to split in chunks, pass each chunk through the sliding window function, …"""
 	logger.info("Preparing LSTM training data...")
 	c = LSTMConfig(config)
 
-	x_train, y_train = to_x_y(df, c)
-	model = LSTMModel(vars_in=len(c.features_in), vars_out=len(c.features_out))
+	x_train, y_train = to_xy(df, c)
+	model = LSTMModel(c)
 
-	#x_train, y_train = prepare_window_off_by_1(df.dropna(), 10)
-	#model = LSTMModel(4, 4)
+	# x_train, y_train = prepare_window_off_by_1(df.dropna(), 10)
+	# model = LSTMModel(4, 4)
 
 	timer = Timer()
 	logger.info("Training LSTM...")
@@ -200,7 +222,7 @@ def test_lstm_model():
 	data_cos = np.cos(np.linspace(start, stop, n)) + (np.random.random(n) * 0.4 - 0.2)
 
 	data_res = 1.0 / (1.0 + np.cos(np.linspace(start, stop, n)) ** 2) + (np.random.random(n) * 0.2 - 0.1)
-	data = pd.DataFrame([data_sin, data_cos, data_res], index=["sin", "cos", "res"]).transpose()
+	data = DataFrame([data_sin, data_cos, data_res], index=["sin", "cos", "res"]).transpose()
 
 	# For fitting, we need need x.shape == [n, v, f] and y.shape == [n, v, f] where:
 	# - n is the number of samples
@@ -214,8 +236,8 @@ def test_lstm_model():
 	model = train_lstm_model_predict(conf['lstm'], data)
 
 	x_test, y_test = prepare_window_off_by_1(data[-50:], 20)
-	y_pred = np.array([model.predict_next(x_test[i]) for i in range(x_test.shape[0])])  # model.predict(x_test, fh=10)
-	y_pred_seq = model.predict_sequence(x_test[0], y_test.shape[0])
+	y_pred = np.array([model.predict_next_n(x_test[i]) for i in range(x_test.shape[0])])  # model.predict(x_test, fh=10)
+	y_pred_seq = model.predict_sequence_n(x_test[0], y_test.shape[0])
 
 	plt.plot(y_test[:, -1, 0])  # blue
 	plt.plot(y_pred[:, 0])  # green
--- src/utils/keras.py
+++ src/utils/keras.py
@@ -3,19 +3,20 @@ from tqdm import tqdm
 
 
 class KerasTrainCallback(Callback):
-	def __init__(self, progress: tqdm):
+	def __init__(self, name: str, progress: tqdm):
 		super().__init__()
+		self.name = name
 		self.progress = progress
 
 	def on_epoch_end(self, epoch, logs=None):
 		acc, loss = logs.get('acc'), logs.get('loss')
 		val_acc, val_loss = logs.get('val_acc'), logs.get('val_loss')
-		line = f'Epoch {epoch} - train loss: {loss:.4f}'
+		line = f'\n{self.name}: Epoch {epoch + 1} - train loss: {loss:.4g}'
 		if acc is not None:
 			line += f' acc: {acc:.2%}'
 		if val_loss is not None:
-			line += f' - validation loss: {val_loss:.4f}'
+			line += f' - validation loss: {val_loss:.4g}'
 			if val_acc is not None:
 				line += f' acc: {val_acc:.2%}'
-		tqdm.write(line)
+		print(line)
 		self.progress.update()
